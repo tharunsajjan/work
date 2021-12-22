51032a50d8361cc7d2db2ff0e8b67d52a0792d18
tharunsajjan <68546601+tharunsajjan@users.noreply.github.com>
2021-12-22 18:39:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
   public static void main(String[] args){
-     System.out.println("Welcome to Jenkins Training. Happy Learning! welcome to world of GIT and devops");
+     System.out.println("Welcome to Jenkins Training. Happy Learning! welcome to world of GIT and devops. New technology");
   }
 } 
