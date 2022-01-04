1cf1d19f34dd88a7449395210fa58f4a7627270d
tharunsajjan <tharun.hyd@gmail.com>
2022-01-04 19:03:39 +0530

    hi

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
   public static void main(String[] args){
-     System.out.println("Welcome to Jenkins Training. Happy Learning! welcome to world of GIT and devops. New technology");
+     System.out.println("Welcome to Jenkins Training. Happy Learning! welcome to world of GIT and devops. Happy learning New technology");
   }
 } 
